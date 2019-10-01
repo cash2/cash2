48eacefd5862626113bae4db7e0f33d8a11a9624
aphivantrakul <aphivantrakul@gmail.com>
2019-10-01 19:39:53 -0400

    Daemon : Moved daemon configuration options into a separate class called DaemonConfigurationOptions files
    Refactored Daemon code and moved daemon configuration options to a separate class outside of the main function.
    
    Reason for changes : to reduce clutter in the main function and to better separate parts of the daemon program into different classes.

---
+++ src/Daemon/DaemonConfigurationOptions.h
@@ -0,0 +1,34 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Copyright (c) 2016-2018, The Karbo developers
+// Copyright (c) 2018-2019 The Cash2 developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+#include <boost/program_options.hpp>
+
+namespace CryptoNote {
+
+class DaemonConfigurationOptions {
+public:
+
+  DaemonConfigurationOptions();
+
+  static void initOptions(boost::program_options::options_description& desc);
+  void init(const boost::program_options::variables_map& options);
+
+  std::string configFile;
+  std::string dataDirectory;
+  std::string enableCors; // should be DaemonRpcConfig parameter instead
+  bool help;
+  std::string logFile;
+  uint32_t logLevel;
+  bool console;
+  bool osVersion;
+  bool printGenesisTransaction;
+  bool restrictedRpc; // should be DaemonRpcConfig parameter instead
+  bool testnet;
+};
+
+}
--- src/Daemon/main.cpp
+++ src/Daemon/main.cpp
@@ -4,106 +4,31 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "version.h"
-
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 
-#include "DaemonCommandsHandler.h"
-
-#include "Common/SignalHandler.h"
 #include "Common/PathTools.h"
-#include "crypto/hash.h"
+#include "Common/SignalHandler.h"
 #include "CryptoNoteCore/Core.h"
 #include "CryptoNoteCore/CoreConfig.h"
 #include "CryptoNoteCore/CryptoNoteTools.h"
 #include "CryptoNoteCore/Currency.h"
 #include "CryptoNoteCore/MinerConfig.h"
 #include "CryptoNoteProtocol/CryptoNoteProtocolHandler.h"
-#include "P2p/NodeServer.h"
-#include "P2p/NodeServerConfig.h"
+#include "DaemonCommandsHandler.h"
+#include "DaemonConfigurationOptions.h"
 #include "DaemonRpcServer.h"
 #include "DaemonRpcServerConfig.h"
-#include "version.h"
-
 #include "Logging/ConsoleLogger.h"
-#include <Logging/LoggerManager.h>
+#include "Logging/LoggerManager.h"
+#include "P2p/NodeServer.h"
+#include "P2p/NodeServerConfig.h"
+#include "crypto/hash.h"
+#include "version.h"
 
 #if defined(WIN32)
 #include <crtdbg.h>
 #endif
-
-using Common::JsonValue;
-using namespace CryptoNote;
-using namespace Logging;
-
-namespace po = boost::program_options;
-
-namespace
-{
-
-const command_line::arg_descriptor<std::string> arg_config_file =       {"config-file", "Specify the configuration file", std::string(CryptoNote::CRYPTONOTE_NAME) + ".conf"};
-const command_line::arg_descriptor<std::string> arg_enable_cors =       {"enable-cors", "Adds header 'Access-Control-Allow-Origin' to Daemon RPC responses. Uses the value as domain. Use * for all", "" };
-const command_line::arg_descriptor<std::string> arg_log_file =          {"log-file", "", ""};
-const command_line::arg_descriptor<int>         arg_log_level =         {"log-level", "", 2}; // info level
-const command_line::arg_descriptor<bool>        arg_console =           {"no-console", "Disable daemon console commands"};
-const command_line::arg_descriptor<bool>        arg_os_version =        {"os-version", "Shows the Cash2 software version and the OS version"};
-const command_line::arg_descriptor<bool>        arg_print_genesis_tx =  {"print-genesis-tx", "Prints genesis block's coinbase transaction as hexidecimal to insert it to config" };
-const command_line::arg_descriptor<bool>        arg_restricted_rpc =    {"restricted-rpc", "Restrict Daemon RPC commands to view only commands to prevent abuse"};
-const command_line::arg_descriptor<bool>        arg_testnet_on =        {"testnet", "Used to deploy test nets. Checkpoints and hardcoded seeds are ignored, network id is changed. Use it with --data-dir flag. The wallet must be launched with --testnet flag.", false};
-
-bool command_line_preprocessor(const boost::program_options::variables_map &vm, LoggerRef &logger) {
-  bool exit = false;
-
-  if (command_line::get_arg(vm, command_line::arg_version)) {
-    std::cout << CryptoNote::CRYPTONOTE_NAME << " v" << PROJECT_VERSION_LONG << ENDL;
-    exit = true;
-  }
-  if (command_line::get_arg(vm, arg_os_version)) {
-    std::cout << "OS: " << Tools::get_os_version_string() << ENDL;
-    exit = true;
-  }
-
-  if (exit) {
-    return true;
-  }
-
-  return false;
-}
-
-void print_genesis_tx_hex() {
-  Logging::ConsoleLogger logger;
-  CryptoNote::Transaction tx = CryptoNote::CurrencyBuilder(logger).generateGenesisTransaction();
-  CryptoNote::BinaryArray txb = CryptoNote::toBinaryArray(tx);
-  std::string tx_hex = Common::toHex(txb);
-
-  std::cout << "Insert this line into your coin configuration file as is: " << std::endl;
-  std::cout << "const char GENESIS_COINBASE_TX_HEX[] = \"" << tx_hex << "\";" << std::endl;
-
-  return;
-}
-
-JsonValue buildLoggerConfiguration(Level level, const std::string& logfile) {
-  JsonValue loggerConfiguration(JsonValue::OBJECT);
-  loggerConfiguration.insert("globalLevel", static_cast<int64_t>(level));
-
-  JsonValue& cfgLoggers = loggerConfiguration.insert("loggers", JsonValue::ARRAY);
-
-  JsonValue& fileLogger = cfgLoggers.pushBack(JsonValue::OBJECT);
-  fileLogger.insert("type", "file");
-  fileLogger.insert("filename", logfile);
-  fileLogger.insert("level", static_cast<int64_t>(TRACE));
-
-  JsonValue& consoleLogger = cfgLoggers.pushBack(JsonValue::OBJECT);
-  consoleLogger.insert("type", "console");
-  consoleLogger.insert("level", static_cast<int64_t>(TRACE));
-  consoleLogger.insert("pattern", "%T %L ");
-
-  return loggerConfiguration;
-}
-
-} // end anonymous namespace
-
 
 int main(int argc, char* argv[])
 {
@@ -112,77 +37,77 @@ int main(int argc, char* argv[])
   _CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
 #endif
 
-  LoggerManager logManager;
-  LoggerRef logger(logManager, "daemon");
+  Logging::LoggerManager logManager;
+  Logging::LoggerRef logger(logManager, "Daemon");
 
   try {
 
-    po::options_description desc_daemon("Daemon Options");
-    po::options_description desc_daemon_rpc("Daemon RPC Server Options");
-    po::options_description desc_core("Core Options");
-    po::options_description desc_network("Network Options");
-    po::options_description desc_mining("Mining Options");
+    boost::program_options::options_description daemonConfigurationOptionsDescription("Daemon Configuration Options");
+    CryptoNote::DaemonConfigurationOptions::initOptions(daemonConfigurationOptionsDescription);
 
-    command_line::add_arg(desc_daemon, arg_config_file);
-    command_line::add_arg(desc_daemon, command_line::arg_data_dir, Tools::getDefaultDataDirectory());
-    command_line::add_arg(desc_daemon, arg_enable_cors);
-    command_line::add_arg(desc_daemon, command_line::arg_help);
-    command_line::add_arg(desc_daemon, arg_log_file);
-    command_line::add_arg(desc_daemon, arg_log_level);
-    command_line::add_arg(desc_daemon, arg_os_version);
-    command_line::add_arg(desc_daemon, arg_console);
-    command_line::add_arg(desc_daemon, arg_print_genesis_tx);
-    command_line::add_arg(desc_daemon, arg_restricted_rpc);
-    command_line::add_arg(desc_daemon, arg_testnet_on);
-    command_line::add_arg(desc_daemon, command_line::arg_version);
+    boost::program_options::options_description daemonRpcServerConfigurationOptionsDescription("Daemon RPC Server Configuration Options");
+    CryptoNote::DaemonRpcServerConfig::initOptions(daemonRpcServerConfigurationOptionsDescription);
 
-    DaemonRpcServerConfig::initOptions(desc_daemon_rpc);
-    CoreConfig::initOptions(desc_core); // There are currently no core configuration options
-    NodeServerConfig::initOptions(desc_network);
-    MinerConfig::initOptions(desc_mining);
+    boost::program_options::options_description coreConfigurationOptionsDescription("Core Configuration Options");
+    CryptoNote::CoreConfig::initOptions(coreConfigurationOptionsDescription);
 
-    po::options_description desc_options("Command Line Options");
-    desc_options.add(desc_daemon).add(desc_daemon_rpc).add(desc_network).add(desc_mining);
+    boost::program_options::options_description nodeServerConfigurationOptionsDescription("Node Server Configuration Options");
+    CryptoNote::NodeServerConfig::initOptions(nodeServerConfigurationOptionsDescription);
 
-    po::variables_map vm;
-    bool r = command_line::handle_error_helper(desc_options, [&]()
+    boost::program_options::options_description miningConfigurationOptionsDescription("Mining Configuration Options");
+    CryptoNote::MinerConfig::initOptions(miningConfigurationOptionsDescription);
+
+    boost::program_options::options_description allConfigurationOptionsDescription("All Configuration Options");
+
+    allConfigurationOptionsDescription.
+      add(daemonConfigurationOptionsDescription).
+      add(daemonRpcServerConfigurationOptionsDescription).
+      add(nodeServerConfigurationOptionsDescription).
+      add(miningConfigurationOptionsDescription);
+
+    boost::program_options::variables_map vm;
+
+    boost::program_options::store(boost::program_options::parse_command_line(argc, argv, allConfigurationOptionsDescription), vm);
+
+    CryptoNote::DaemonConfigurationOptions daemonConfigurationOptions;
+    daemonConfigurationOptions.init(vm);
+
+    if (daemonConfigurationOptions.help)
     {
-      po::store(po::parse_command_line(argc, argv, desc_options), vm);
+      std::cout << CryptoNote::CRYPTONOTE_NAME << " v" << PROJECT_VERSION_LONG << ENDL << ENDL;
+      std::cout << allConfigurationOptionsDescription << std::endl;
+      return 0;
+    }
 
-      if (command_line::get_arg(vm, command_line::arg_help))
-      {
-        std::cout << CryptoNote::CRYPTONOTE_NAME << " v" << PROJECT_VERSION_LONG << ENDL << ENDL;
-        std::cout << desc_options << std::endl;
-        return false;
-      }
+    if (daemonConfigurationOptions.printGenesisTransaction) {
+      Logging::ConsoleLogger logger;
+      CryptoNote::Transaction tx = CryptoNote::CurrencyBuilder(logger).generateGenesisTransaction();
+      CryptoNote::BinaryArray txb = CryptoNote::toBinaryArray(tx);
+      std::string tx_hex = Common::toHex(txb);
 
-      if (command_line::get_arg(vm, arg_print_genesis_tx)) {
-        print_genesis_tx_hex();
-        return false;
-      }
+      std::cout << "Insert this line into your coin configuration file as is: " << std::endl;
+      std::cout << "const char GENESIS_COINBASE_TX_HEX[] = \"" << tx_hex << "\";" << std::endl;
+      return 0;
+    }
 
-      std::string data_dir = command_line::get_arg(vm, command_line::arg_data_dir);
-      std::string config = command_line::get_arg(vm, arg_config_file);
+    std::string dataDirectory = daemonConfigurationOptions.dataDirectory;
+    std::string configFile = daemonConfigurationOptions.configFile;
 
-      boost::filesystem::path data_dir_path(data_dir);
-      boost::filesystem::path config_path(config);
-      if (!config_path.has_parent_path()) {
-        config_path = data_dir_path / config_path;
-      }
+    boost::filesystem::path dataDirectoryPath(dataDirectory);
+    boost::filesystem::path configFilePath(configFile);
+    if (!configFilePath.has_parent_path()) {
+      configFilePath = dataDirectoryPath / configFilePath;
+    }
 
-      boost::system::error_code ec;
-      if (boost::filesystem::exists(config_path, ec)) {
-        po::store(po::parse_config_file<char>(config_path.string<std::string>().c_str(), desc_daemon), vm);
-      }
-      po::notify(vm);
-      return true;
-    });
+    boost::system::error_code ec;
+    if (boost::filesystem::exists(configFilePath, ec)) {
+      boost::program_options::store(boost::program_options::parse_config_file<char>(configFilePath.string<std::string>().c_str(), daemonConfigurationOptionsDescription), vm);
+    }
 
-    if (!r)
-      return 1;
-  
+    boost::program_options::notify(vm);
+
     std::string modulePath = Common::NativePathToGeneric(argv[0]);
-    std::string logFile = Common::NativePathToGeneric(command_line::get_arg(vm, arg_log_file));
+    std::string logFile = Common::NativePathToGeneric(daemonConfigurationOptions.logFile);
 
     if (logFile.empty()) {
       logFile = Common::ReplaceExtenstion(modulePath, ".log");
@@ -192,32 +117,52 @@ int main(int argc, char* argv[])
       }
     }
 
-    Level logLevel = static_cast<Level>(static_cast<int>(Logging::ERROR) + command_line::get_arg(vm, arg_log_level));
+    Logging::Level logLevel = static_cast<Logging::Level>(static_cast<int>(Logging::ERROR) + daemonConfigurationOptions.logLevel);
 
     // configure logging
-    logManager.configure(buildLoggerConfiguration(logLevel, logFile));
+    Common::JsonValue loggerConfiguration(Common::JsonValue::OBJECT);
+    loggerConfiguration.insert("globalLevel", static_cast<int64_t>(logLevel));
 
-    logger(INFO) << CryptoNote::CRYPTONOTE_NAME << " v" << PROJECT_VERSION_LONG;
+    Common::JsonValue& cfgLoggers = loggerConfiguration.insert("loggers", Common::JsonValue::ARRAY);
 
-    if (command_line_preprocessor(vm, logger)) {
+    Common::JsonValue& fileLogger = cfgLoggers.pushBack(Common::JsonValue::OBJECT);
+    fileLogger.insert("type", "file");
+    fileLogger.insert("filename", logFile);
+    fileLogger.insert("level", static_cast<int64_t>(Logging::TRACE));
+
+    Common::JsonValue& consoleLogger = cfgLoggers.pushBack(Common::JsonValue::OBJECT);
+    consoleLogger.insert("type", "console");
+    consoleLogger.insert("level", static_cast<int64_t>(Logging::TRACE));
+    consoleLogger.insert("pattern", "%T %L ");
+    logManager.configure(loggerConfiguration);
+    // end configure logging
+
+    logger(Logging::INFO) << CryptoNote::CRYPTONOTE_NAME << " v" << PROJECT_VERSION_LONG;
+
+    if (command_line::get_arg(vm, command_line::arg_version)) {
+      std::cout << CryptoNote::CRYPTONOTE_NAME << " v" << PROJECT_VERSION_LONG << ENDL;
       return 0;
     }
 
-    logger(INFO) << "Module folder: " << argv[0];
-
-    bool testnet_mode = command_line::get_arg(vm, arg_testnet_on);
-    if (testnet_mode) {
-      logger(INFO) << "Starting in testnet mode!";
+    if (daemonConfigurationOptions.osVersion) {
+      std::cout << "OS version : " << Tools::get_os_version_string() << ENDL;
+      return 0;
     }
 
-    //create objects and link them
+    logger(Logging::INFO) << "Module folder : " << argv[0];
+
+    bool testnet_mode = daemonConfigurationOptions.testnet;
+    if (testnet_mode) {
+      logger(Logging::INFO) << "Starting in testnet mode ...";
+    }
+
     CryptoNote::CurrencyBuilder currencyBuilder(logManager);
     currencyBuilder.testnet(testnet_mode);
 
     try {
       currencyBuilder.currency();
     } catch (std::exception&) {
-      std::cout << "GENESIS_COINBASE_TX_HEX constant has an incorrect value. Please launch: " << CryptoNote::CRYPTONOTE_NAME << "d --" << arg_print_genesis_tx.name;
+      std::cout << "GENESIS_COINBASE_TX_HEX constant has an incorrect value. Please launch : " << CryptoNote::CRYPTONOTE_NAME << "d --print-genesis-tx";
       return 1;
     }
 
@@ -233,23 +178,16 @@ int main(int argc, char* argv[])
       core.set_checkpoints(std::move(checkpoints));
     }
 
-    CoreConfig coreConfig;
+    CryptoNote::CoreConfig coreConfig;
     coreConfig.init(vm);
-    NodeServerConfig nodeServerConfig;
-    nodeServerConfig.init(vm);
-    nodeServerConfig.setTestnet(testnet_mode);
-    MinerConfig minerConfig;
-    minerConfig.init(vm);
-    DaemonRpcServerConfig daemonRpcServerConfig;
-    daemonRpcServerConfig.init(vm);
 
     if (!coreConfig.configFolderDefaulted) {
       if (!Tools::directoryExists(coreConfig.configFolder)) {
-        throw std::runtime_error("Directory does not exist: " + coreConfig.configFolder);
+        throw std::runtime_error("Directory does not exist : " + coreConfig.configFolder);
       }
     } else {
       if (!Tools::create_directories_if_necessary(coreConfig.configFolder)) {
-        throw std::runtime_error("Can't create directory: " + coreConfig.configFolder);
+        throw std::runtime_error("Can't create directory : " + coreConfig.configFolder);
       }
     }
 
@@ -257,6 +195,9 @@ int main(int argc, char* argv[])
 
     CryptoNote::CryptoNoteProtocolHandler cryptoNoteProtocolHandler(currency, dispatcher, core, nullptr, logManager);
     CryptoNote::NodeServer nodeServer(dispatcher, cryptoNoteProtocolHandler, logManager);
+
+    CryptoNote::DaemonRpcServerConfig daemonRpcServerConfig;
+    daemonRpcServerConfig.init(vm);
     CryptoNote::DaemonRpcServer daemonRpcServer(dispatcher, logManager, core, nodeServer, cryptoNoteProtocolHandler, daemonRpcServerConfig);
 
     cryptoNoteProtocolHandler.set_p2p_endpoint(&nodeServer);
@@ -264,61 +205,68 @@ int main(int argc, char* argv[])
     DaemonCommandsHandler daemonCommandsHandler(core, nodeServer, logManager);
 
     // initialize objects
-    logger(INFO) << "Initializing node server...";
+    CryptoNote::NodeServerConfig nodeServerConfig;
+    nodeServerConfig.init(vm);
+    nodeServerConfig.setTestnet(testnet_mode);
+
+    logger(Logging::INFO) << "Initializing Node Server ...";
     if (!nodeServer.init(nodeServerConfig)) {
-      logger(ERROR, BRIGHT_RED) << "Failed to initialize node server.";
+      logger(Logging::ERROR, Logging::BRIGHT_RED) << "Failed to initialize Node Server";
       return 1;
     }
-    logger(INFO) << "Node server initialized OK";
+    logger(Logging::INFO) << "Node Server initialized OK";
 
     // initialize core here
-    logger(INFO) << "Initializing core...";
+    CryptoNote::MinerConfig minerConfig;
+    minerConfig.init(vm);
+
+    logger(Logging::INFO) << "Initializing Core ...";
     if (!core.init(coreConfig, minerConfig, true)) {
-      logger(ERROR, BRIGHT_RED) << "Failed to initialize core";
+      logger(Logging::ERROR, Logging::BRIGHT_RED) << "Failed to initialize Core";
       return 1;
     }
-    logger(INFO) << "Core initialized OK";
+    logger(Logging::INFO) << "Core initialized OK";
 
     // start components
-    if (!command_line::has_arg(vm, arg_console)) {
+    if (!daemonConfigurationOptions.console) {
       daemonCommandsHandler.start_handling();
     }
 
-    logger(INFO) << "Starting core rpc server on address " << daemonRpcServerConfig.getBindAddress();
+    logger(Logging::INFO) << "Starting Daemon RPC Server on network address " << daemonRpcServerConfig.getBindAddress() << " ...";
     daemonRpcServer.start();
-    daemonRpcServer.setRestrictedRpc(command_line::get_arg(vm, arg_restricted_rpc));
-    daemonRpcServer.enableCors(command_line::get_arg(vm, arg_enable_cors));
-    logger(INFO) << "Core rpc server started ok";
+    daemonRpcServer.setRestrictedRpc(daemonConfigurationOptions.restrictedRpc);
+    daemonRpcServer.enableCors(daemonConfigurationOptions.enableCors);
+    logger(Logging::INFO) << "Daemon RPC Server started OK";
 
     Tools::SignalHandler::install([&daemonCommandsHandler, &nodeServer] {
       daemonCommandsHandler.stop_handling();
       nodeServer.sendStopSignal();
     });
 
-    logger(INFO) << "Starting node server net loop...";
+    logger(Logging::INFO) << "Starting Node Server ...";
     nodeServer.run();
-    logger(INFO) << "node server net loop stopped";
+    logger(Logging::INFO) << "Node Server stopped";
 
     daemonCommandsHandler.stop_handling();
 
     //stop components
-    logger(INFO) << "Stopping daemon RPC server...";
+    logger(Logging::INFO) << "Stopping Daemon RPC Server ...";
     daemonRpcServer.stop();
 
     //deinitialize components
-    logger(INFO) << "Deinitializing core...";
+    logger(Logging::INFO) << "Deinitializing Core ...";
     core.deinit();
-    logger(INFO) << "Deinitializing node server...";
+    logger(Logging::INFO) << "Deinitializing Node Server ...";
     nodeServer.deinit();
 
     core.set_cryptonote_protocol(NULL);
     cryptoNoteProtocolHandler.set_p2p_endpoint(NULL);
 
   } catch (const std::exception& e) {
-    logger(ERROR, BRIGHT_RED) << "Exception: " << e.what();
+    logger(Logging::ERROR, Logging::BRIGHT_RED) << "Exception : " << e.what();
     return 1;
   }
 
-  logger(INFO) << "Node stopped.";
+  logger(Logging::INFO) << "Node stopped";
   return 0;
 }
