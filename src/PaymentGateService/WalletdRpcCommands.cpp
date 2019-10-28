c635c42ee1f00908623b191fb33c4cacb05bb13b
aphivantrakul <aphivantrakul@gmail.com>
2019-10-28 18:50:23 -0400

    Walletd : Alphabetized functions in WalletdRpcCommands.cpp
    Reason for changes : to better organize the code to improve readability

--- src/PaymentGateService/WalletdRpcCommands.cpp
+++ src/PaymentGateService/WalletdRpcCommands.cpp
@@ -12,113 +12,152 @@ namespace PaymentService {
 
 WalletdRpcCommands::WalletdRpcCommands(WalletService& walletService) : service(walletService) {}
 
-std::error_code WalletdRpcCommands::reset(const WALLETD_RPC_COMMAND_RESET::Request& request, WALLETD_RPC_COMMAND_RESET::Response& response) {
-  if (request.view_private_key.empty()) {
-    return service.resetWallet();
-  } else {
-    return service.replaceWithNewWallet(request.view_private_key);
-  }
-}
-
-std::error_code WalletdRpcCommands::createAddress(const WALLETD_RPC_COMMAND_CREATE_ADDRESS::Request& request, WALLETD_RPC_COMMAND_CREATE_ADDRESS::Response& response) {
-  if (request.spend_private_key.empty() && request.spend_public_key.empty()) {
+std::error_code WalletdRpcCommands::createAddress(const WALLETD_RPC_COMMAND_CREATE_ADDRESS::Request& request, WALLETD_RPC_COMMAND_CREATE_ADDRESS::Response& response)
+{
+  if (request.spend_private_key.empty() && request.spend_public_key.empty())
+  {
     return service.createAddress(response.address);
-  } else if (!request.spend_private_key.empty()) {
+  }
+  else if (!request.spend_private_key.empty())
+  {
     return service.createAddress(request.spend_private_key, response.address);
-  } else {
+  }
+  else
+  {
     return service.createTrackingAddress(request.spend_public_key, response.address);
   }
 }
 
-std::error_code WalletdRpcCommands::deleteAddress(const WALLETD_RPC_COMMAND_DELETE_ADDRESS::Request& request, WALLETD_RPC_COMMAND_DELETE_ADDRESS::Response& response) {
+std::error_code WalletdRpcCommands::createDelayedTransaction(const WALLETD_RPC_COMMAND_CREATE_DELAYED_TRANSACTION::Request& request, WALLETD_RPC_COMMAND_CREATE_DELAYED_TRANSACTION::Response& response)
+{
+  return service.createDelayedTransaction(request, response.transaction_hash);
+}
+
+std::error_code WalletdRpcCommands::deleteAddress(const WALLETD_RPC_COMMAND_DELETE_ADDRESS::Request& request, WALLETD_RPC_COMMAND_DELETE_ADDRESS::Response& response)
+{
   return service.deleteAddress(request.address);
 }
 
-std::error_code WalletdRpcCommands::getSpendPrivateKey(const WALLETD_RPC_COMMAND_GET_SPEND_PRIVATE_KEY::Request& request, WALLETD_RPC_COMMAND_GET_SPEND_PRIVATE_KEY::Response& response) {
-  return service.getSpendPrivateKey(request.address, response.spend_private_key);
+std::error_code WalletdRpcCommands::deleteDelayedTransaction(const WALLETD_RPC_COMMAND_DELETE_DELAYED_TRANSACTION::Request& request, WALLETD_RPC_COMMAND_DELETE_DELAYED_TRANSACTION::Response& response)
+{
+  return service.deleteDelayedTransaction(request.transaction_hash);
 }
 
-std::error_code WalletdRpcCommands::getSpendPrivateKeys(const WALLETD_RPC_COMMAND_GET_SPEND_PRIVATE_KEYS::Request& request, WALLETD_RPC_COMMAND_GET_SPEND_PRIVATE_KEYS::Response& response) {
-  return service.getSpendPrivateKeys(response.spend_private_keys);
+std::error_code WalletdRpcCommands::getAddresses(const WALLETD_RPC_COMMAND_GET_ADDRESSES::Request& request, WALLETD_RPC_COMMAND_GET_ADDRESSES::Response& response)
+{
+  return service.getAddresses(response.addresses);
 }
 
-std::error_code WalletdRpcCommands::getBalance(const WALLETD_RPC_COMMAND_GET_BALANCE::Request& request, WALLETD_RPC_COMMAND_GET_BALANCE::Response& response) {
-  if (!request.address.empty()) {
+std::error_code WalletdRpcCommands::getAddressesCount(const WALLETD_RPC_COMMAND_GET_ADDRESSES_COUNT::Request& request, WALLETD_RPC_COMMAND_GET_ADDRESSES_COUNT::Response& response)
+{
+  return service.getAddressesCount(response.addresses_count);
+}
+
+std::error_code WalletdRpcCommands::getBalance(const WALLETD_RPC_COMMAND_GET_BALANCE::Request& request, WALLETD_RPC_COMMAND_GET_BALANCE::Response& response)
+{
+  if (!request.address.empty())
+  {
     return service.getBalance(request.address, response.available_balance, response.locked_amount);
-  } else {
+  }
+  else
+  {
     return service.getBalance(response.available_balance, response.locked_amount);
   }
 }
 
-std::error_code WalletdRpcCommands::getBlockHashes(const WALLETD_RPC_COMMAND_GET_BLOCK_HASHES::Request& request, WALLETD_RPC_COMMAND_GET_BLOCK_HASHES::Response& response) {
+std::error_code WalletdRpcCommands::getBlockHashes(const WALLETD_RPC_COMMAND_GET_BLOCK_HASHES::Request& request, WALLETD_RPC_COMMAND_GET_BLOCK_HASHES::Response& response)
+{
   uint32_t startBlockIndex = request.start_block_height - 1;
   return service.getBlockHashes(startBlockIndex, request.number_of_blocks, response.block_hashes);
 }
 
-std::error_code WalletdRpcCommands::getTransactionHashes(const WALLETD_RPC_COMMAND_GET_TRANSACTION_HASHES::Request& request, WALLETD_RPC_COMMAND_GET_TRANSACTION_HASHES::Response& response) {
-  if (!request.start_block_hash.empty()) {
+std::error_code WalletdRpcCommands::getDelayedTransactionHashes(const WALLETD_RPC_COMMAND_GET_DELAYED_TRANSACTION_HASHES::Request& request, WALLETD_RPC_COMMAND_GET_DELAYED_TRANSACTION_HASHES::Response& response)
+{
+  return service.getDelayedTransactionHashes(response.transaction_hashes);
+}
+
+std::error_code WalletdRpcCommands::getSpendPrivateKey(const WALLETD_RPC_COMMAND_GET_SPEND_PRIVATE_KEY::Request& request, WALLETD_RPC_COMMAND_GET_SPEND_PRIVATE_KEY::Response& response)
+{
+  return service.getSpendPrivateKey(request.address, response.spend_private_key);
+}
+
+std::error_code WalletdRpcCommands::getSpendPrivateKeys(const WALLETD_RPC_COMMAND_GET_SPEND_PRIVATE_KEYS::Request& request, WALLETD_RPC_COMMAND_GET_SPEND_PRIVATE_KEYS::Response& response)
+{
+  return service.getSpendPrivateKeys(response.spend_private_keys);
+}
+
+std::error_code WalletdRpcCommands::getStatus(const WALLETD_RPC_COMMAND_GET_STATUS::Request& request, WALLETD_RPC_COMMAND_GET_STATUS::Response& response)
+{
+  response.cash2_software_version = PROJECT_VERSION_LONG;
+
+  return service.getStatus(response.block_count, response.known_block_count, response.last_block_hash, response.peer_count, response.minimal_fee);
+}
+
+std::error_code WalletdRpcCommands::getTransaction(const WALLETD_RPC_COMMAND_GET_TRANSACTION::Request& request, WALLETD_RPC_COMMAND_GET_TRANSACTION::Response& response)
+{
+  return service.getTransaction(request.transaction_hash, response.transaction);
+}
+
+std::error_code WalletdRpcCommands::getTransactionHashes(const WALLETD_RPC_COMMAND_GET_TRANSACTION_HASHES::Request& request, WALLETD_RPC_COMMAND_GET_TRANSACTION_HASHES::Response& response)
+{
+  if (!request.start_block_hash.empty())
+  {
     return service.getTransactionHashes(request.addresses, request.start_block_hash, request.number_of_blocks, request.payment_id, response.items);
-  } else {
+  }
+  else
+  {
     uint32_t startBlockIndex = request.start_block_height - 1;
     return service.getTransactionHashes(request.addresses, startBlockIndex, request.number_of_blocks, request.payment_id, response.items);
   }
 }
 
-std::error_code WalletdRpcCommands::getTransactions(const WALLETD_RPC_COMMAND_GET_TRANSACTIONS::Request& request, WALLETD_RPC_COMMAND_GET_TRANSACTIONS::Response& response) {
-  if (!request.start_block_hash.empty()) {
+std::error_code WalletdRpcCommands::getTransactions(const WALLETD_RPC_COMMAND_GET_TRANSACTIONS::Request& request, WALLETD_RPC_COMMAND_GET_TRANSACTIONS::Response& response)
+{
+  if (!request.start_block_hash.empty())
+  {
     return service.getTransactions(request.addresses, request.start_block_hash, request.number_of_blocks, request.payment_id, response.items);
-  } else {
+  }
+  else
+  {
     uint32_t startBlockIndex = request.start_block_height - 1;
     return service.getTransactions(request.addresses, startBlockIndex, request.number_of_blocks, request.payment_id, response.items);
   }
 }
 
-std::error_code WalletdRpcCommands::getUnconfirmedTransactionHashes(const WALLETD_RPC_COMMAND_GET_UNCONFIRMED_TRANSACTION_HASHES::Request& request, WALLETD_RPC_COMMAND_GET_UNCONFIRMED_TRANSACTION_HASHES::Response& response) {
+std::error_code WalletdRpcCommands::getUnconfirmedTransactionHashes(const WALLETD_RPC_COMMAND_GET_UNCONFIRMED_TRANSACTION_HASHES::Request& request, WALLETD_RPC_COMMAND_GET_UNCONFIRMED_TRANSACTION_HASHES::Response& response)
+{
   return service.getUnconfirmedTransactionHashes(request.addresses, response.transaction_hashes);
 }
 
-std::error_code WalletdRpcCommands::getTransaction(const WALLETD_RPC_COMMAND_GET_TRANSACTION::Request& request, WALLETD_RPC_COMMAND_GET_TRANSACTION::Response& response) {
-  return service.getTransaction(request.transaction_hash, response.transaction);
-}
-
-std::error_code WalletdRpcCommands::sendTransaction(const WALLETD_RPC_COMMAND_SEND_TRANSACTION::Request& request, WALLETD_RPC_COMMAND_SEND_TRANSACTION::Response& response) {
-  return service.sendTransaction(request, response.transaction_hash, response.transaction_private_key);
-}
-
-std::error_code WalletdRpcCommands::createDelayedTransaction(const WALLETD_RPC_COMMAND_CREATE_DELAYED_TRANSACTION::Request& request, WALLETD_RPC_COMMAND_CREATE_DELAYED_TRANSACTION::Response& response) {
-  return service.createDelayedTransaction(request, response.transaction_hash);
-}
-
-std::error_code WalletdRpcCommands::getDelayedTransactionHashes(const WALLETD_RPC_COMMAND_GET_DELAYED_TRANSACTION_HASHES::Request& request, WALLETD_RPC_COMMAND_GET_DELAYED_TRANSACTION_HASHES::Response& response) {
-  return service.getDelayedTransactionHashes(response.transaction_hashes);
-}
-
-std::error_code WalletdRpcCommands::deleteDelayedTransaction(const WALLETD_RPC_COMMAND_DELETE_DELAYED_TRANSACTION::Request& request, WALLETD_RPC_COMMAND_DELETE_DELAYED_TRANSACTION::Response& response) {
-  return service.deleteDelayedTransaction(request.transaction_hash);
-}
-
-std::error_code WalletdRpcCommands::sendDelayedTransaction(const WALLETD_RPC_COMMAND_SEND_DELAYED_TRANSACTION::Request& request, WALLETD_RPC_COMMAND_SEND_DELAYED_TRANSACTION::Response& response) {
-  return service.sendDelayedTransaction(request.transaction_hash);
-}
-
-std::error_code WalletdRpcCommands::getViewPrivateKey(const WALLETD_RPC_COMMAND_GET_VIEW_PRIVATE_KEY::Request& request, WALLETD_RPC_COMMAND_GET_VIEW_PRIVATE_KEY::Response& response) {
+std::error_code WalletdRpcCommands::getViewPrivateKey(const WALLETD_RPC_COMMAND_GET_VIEW_PRIVATE_KEY::Request& request, WALLETD_RPC_COMMAND_GET_VIEW_PRIVATE_KEY::Response& response)
+{
   return service.getViewPrivateKey(response.view_private_key);
 }
 
-std::error_code WalletdRpcCommands::getStatus(const WALLETD_RPC_COMMAND_GET_STATUS::Request& request, WALLETD_RPC_COMMAND_GET_STATUS::Response& response) {
-  response.cash2_software_version = PROJECT_VERSION_LONG;
-  return service.getStatus(response.block_count, response.known_block_count, response.last_block_hash, response.peer_count, response.minimal_fee);
+std::error_code WalletdRpcCommands::reset(const WALLETD_RPC_COMMAND_RESET::Request& request, WALLETD_RPC_COMMAND_RESET::Response& response)
+{
+  if (request.view_private_key.empty())
+  {
+    return service.resetWallet();
+  }
+  else
+  {
+    return service.replaceWithNewWallet(request.view_private_key);
+  }
 }
 
-std::error_code WalletdRpcCommands::getAddresses(const WALLETD_RPC_COMMAND_GET_ADDRESSES::Request& request, WALLETD_RPC_COMMAND_GET_ADDRESSES::Response& response) {
-  return service.getAddresses(response.addresses);
+std::error_code WalletdRpcCommands::sendDelayedTransaction(const WALLETD_RPC_COMMAND_SEND_DELAYED_TRANSACTION::Request& request, WALLETD_RPC_COMMAND_SEND_DELAYED_TRANSACTION::Response& response)
+{
+  return service.sendDelayedTransaction(request.transaction_hash);
 }
 
-std::error_code WalletdRpcCommands::getAddressesCount(const WALLETD_RPC_COMMAND_GET_ADDRESSES_COUNT::Request& request, WALLETD_RPC_COMMAND_GET_ADDRESSES_COUNT::Response& response) {
-  return service.getAddressesCount(response.addresses_count);
+std::error_code WalletdRpcCommands::sendTransaction(const WALLETD_RPC_COMMAND_SEND_TRANSACTION::Request& request, WALLETD_RPC_COMMAND_SEND_TRANSACTION::Response& response)
+{
+  return service.sendTransaction(request, response.transaction_hash, response.transaction_private_key);
 }
 
-std::error_code WalletdRpcCommands::validateAddress(const WALLETD_RPC_COMMAND_VALIDATE_ADDRESS::Request& request, WALLETD_RPC_COMMAND_VALIDATE_ADDRESS::Response& response) {
+std::error_code WalletdRpcCommands::validateAddress(const WALLETD_RPC_COMMAND_VALIDATE_ADDRESS::Request& request, WALLETD_RPC_COMMAND_VALIDATE_ADDRESS::Response& response)
+{
   return service.validateAddress(request.address, response.address_valid);
 }
 
